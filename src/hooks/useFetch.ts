08b7de80b63299ebe22543517fa3c0c8de49ecdd
Vinicius Blazius Goulart <viblaziusgoulart@gmail.com>
2022-09-21 15:59:16 -0300

    final project

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -4,14 +4,16 @@ export const useFetch = <T>(url: string) => {
   const [response, setResponse] = useState<T>();
 
   useEffect(() => {
-    const fetchData = async () => {
-      const res = await fetch(url);
-      const data = await res.json();
+    (async () => {
+      try {
+        const res = await fetch(url);
+        const data = await res.json();
 
-      setResponse(data);
-    };
-
-    fetchData();
+        setResponse(data);
+      } catch (err) {
+        console.log(err);
+      }
+    })();
   }, [url]);
 
   return { response };
